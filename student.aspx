9f5a519298cebd27fc582ab9d2e1f8a3c9beb62c
Sameermurtaza12 <107950748+Sameermurtaza12@users.noreply.github.com>
2022-07-29 06:57:59 -0400

    Add files via upload
    data show

---
+++ student.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="student.aspx.cs" Inherits="Task1_student_data.Student_data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Information</title>
+     <style type="text/css">  
+    .list  
+    {  
+        position:absolute;  
+        top:50px;  
+        left:150px;  
+        right:400px;  
+        height:500px;  
+        bottom:100px;  
+        background-color:pink;  
+        }  
+           </style> 
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="list">
+            <asp:DropDownList ID="listDropDown"   runat="server" Width="220px" BackColor="aqua" ></asp:DropDownList>
+            <h2 id="student-data">Please Select the student</h2>
+        </div>
+    </form>
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        const studentList = document.getElementById("StudentListDropDown");
+        studentList.addEventListener("change", () => {
+            var studentName = studentList.value;
+            $.ajax({
+                url: 'student.aspx/GetStudentData',
+                type: 'post',
+                data: JSON.stringify({ "name": studentName }),
+                contentType: 'application/json',
+                async: true,
+                success: function (data) {
+                    var html = `Student Name: ${data.d.Name} <br><hr> Stuent RollNo: ${data.d.RollNo} <br><hr> Student Address: ${data.d.Address} `
+                    $("#student-data").html(html)
+                    console.log(data)
+                }
+            });
+            console.log("Ajax ran");
+        })
+    </script>
+</body>
+</html>
---
+++ student.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+
+namespace Task1_student_data
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+    public partial class Student_data : System.Web.UI.Page
+    {
+        protected static List<Student> studentList;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var s1 = new Student() { RollNo = 0086, Name = "Sameer Murtaza", Address = "House No. 21, Light House Centre, Karachi" };
+            var s2 = new Student() { RollNo = 0284, Name = "Zaryab Ali Rana", Address = "A-1/225, Wapda Town" };
+            var s3 = new Student() { RollNo = 0275, Name = "Rao Usama Abid", Address = "35-Umer Electric Market, Lahore" };
+            var s4 = new Student() { RollNo = 0281, Name = "Shahroz Shahzad", Address = "206-c, Iqbal Garden, Shobraj Chitumal Road" };
+            var s5 = new Student() { RollNo = 0289, Name = "Hadeed Razzaq", Address = "Main Bilal Chowrangi, Sector No. 18, Karachi" };
+            var s6 = new Student() { RollNo = 0204, Name = "Fahad Shakeel", Address = "Hakim & Hashmi Shopping Centre, Preedy Street, Karachi" };
+            var s7 = new Student() { RollNo = 0238, Name = "Syed Ibtesam Hassan", Address = "5-K, Model Town, Lahore" };
+            var s8 = new Student() { RollNo = 0288, Name = "Murtaza Ali", Address = "Hakim & Hashmi Shopping Centre, Preedy Street, Karachi" };
+
+            studentList = new List<Student>() { s1, s2, s3, s4, s5, s6, s7, s8 };
+            listDropDown.DataSource = studentList;
+            listDropDown.DataTextField = "Name";
+            listDropDown.DataValueField = "Name";
+            listDropDown.DataBind();
+
+        }
+        [WebMethod]
+        public static Student GetStudentData(string name)
+        {
+            foreach (var student in studentList)
+            {
+                if (student.Name == name)
+                {
+                    return student;
+                }
+            }
+            return new Student();
+        }
+    }
+}
